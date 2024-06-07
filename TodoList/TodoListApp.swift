5eb419bfe0259dbade342b58a4470d9b5cf7b43f
11 <syjefb2@outlook.com>
2024-06-07 15:19:04 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by 1 1 on 2024/6/7.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
